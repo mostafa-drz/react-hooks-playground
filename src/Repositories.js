b1303c91d6898d604fc5ae48e0460dba512569e9
mostafa.drz <mostafa69d@gmail.com>
2019-05-11 19:24:03 -0400

    more css

--- src/Repositories.js
+++ src/Repositories.js
@@ -18,7 +18,7 @@ function Repositories(props) {
           <h2>Repositories</h2>
           <FixedSizeList
             itemCount={repos.length}
-            height={1000}
+            height={600}
             itemSize={150}
             width="100%"
           >
@@ -71,32 +71,63 @@ function Repo({
   owner: { html_url: ownerURL, avatar_url: ownerAvatar, login: ownerName },
   html_url,
   description,
-  name
+  name,
+  stargazers_count: stars,
+  open_issues_count: issues
 }) {
   return (
-    <Row style={{ border: "2px solid #ccc", marginBottom: "5px" }}>
-      <Col>
-        <div>
+    <Row
+      style={{
+        border: "2px solid #ccc",
+        marginBottom: "5px",
+        maxHeight: "200px"
+      }}
+    >
+      <Col style={{ flexBasis: "70%" }}>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "space-between",
+            height: "100%"
+          }}
+        >
           <a href={html_url} target="_blank" rel="noopener noreferrer">
             <h2 style={{ fontSize: "1.5rem" }}>{name}</h2>
           </a>
           <p style={{ fontSize: "1rem" }}>{description}</p>
+          <div style={{ display: "flex" }}>
+            <div style={{ minWidth: "50px" }}>
+              <span>{stars}</span>
+              <span role="img" aria-label="stars">
+                ⭐️
+              </span>
+            </div>
+            <div style={{ minWidth: "50px" }}>
+              <span>{issues}</span>
+              <span role="img" aria-label="stars">
+                💩
+              </span>
+            </div>
+          </div>
         </div>
       </Col>
-      <Col
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          alignContent: "center",
-          borderLeft: "3px solid #e9e6ee",
-          flex: ".5",
-          marginTop: "3px",
-          marginBottom: "3px"
-        }}
-      >
-        <a href={ownerURL}>
-          <h3 style={{ fontSize: "1rem" }}>{ownerName}</h3>
+      <Col style={{ flexBasis: "30%" }}>
+        <a
+          href={ownerURL}
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+            borderLeft: "3px solid #e9e6ee",
+            marginTop: "3px",
+            marginBottom: "3px"
+          }}
+        >
+          <h3 style={{ fontSize: "1rem", marginBottom: "1rem" }}>
+            {ownerName}
+          </h3>
           <Col xs={6} md={4}>
             <Image
               src={ownerAvatar}
@@ -109,4 +140,5 @@ function Repo({
     </Row>
   );
 }
+
 export default Repositories;
