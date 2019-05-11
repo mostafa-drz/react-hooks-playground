27a83514c8da2a089f1beb2fff828c2738383a23
mostafa.drz <mostafa69d@gmail.com>
2019-05-11 15:48:43 -0400

    basic setup

---
+++ src/Repositories.js
@@ -0,0 +1,75 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Image from "react-bootstrap/Image";
+import { FixedSizeList } from "react-window";
+class Repositories extends React.Component {
+  state = {
+    repos: []
+  };
+  componentDidMount() {
+    this.fetchRepositories();
+  }
+  fetchRepositories = async () => {
+    const resp = await fetch("https://api.github.com/repositories").then(res =>
+      res.json()
+    );
+    this.setState({ repos: resp });
+  };
+  render() {
+    const { repos } = this.state;
+    return (
+      <Container>
+        <h1>Repositories</h1>
+        {repos.map(rep => (
+          <Repo key={rep.id} {...rep} />
+        ))}
+      </Container>
+    );
+  }
+}
+
+function Repo({
+  owner: { url: ownerURL, avatar_url: ownerAvatar, login: ownerName },
+  html_url,
+  description,
+  name
+}) {
+  return (
+    <Row style={{ border: "2px solid #ccc", marginBottom: "5px" }}>
+      <Col>
+        <div>
+          <a href={html_url} target="_blank" rel="noopener noreferrer">
+            <h2 style={{ fontSize: "1.5rem" }}>{name}</h2>
+          </a>
+          <p style={{ fontSize: "1rem" }}>{description}</p>
+        </div>
+      </Col>
+      <Col
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          alignContent: "center",
+          borderLeft: "3px solid #e9e6ee",
+          flex: ".5",
+          marginTop: "3px",
+          marginBottom: "3px"
+        }}
+      >
+        <a href={ownerURL}>
+          <h3 style={{ fontSize: "1rem" }}>{ownerName}</h3>
+          <Col xs={6} md={4}>
+            <Image
+              src={ownerAvatar}
+              roundedCircle
+              style={{ maxWidth: "100px", maxHeight: "100px" }}
+            />
+          </Col>
+        </a>
+      </Col>
+    </Row>
+  );
+}
+export default Repositories;
