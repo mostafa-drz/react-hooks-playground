b7098bd5f3dd88e7b4e5ba6d0fd44e4c5e2de5ea
mostafa.drz <mostafa69d@gmail.com>
2019-05-12 10:20:31 -0400

    refactor the App to useReducer-nice

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,19 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect, useReducer } from "react";
 import "./App.css";
 import Repositories from "./Repositories";
 import UserSearch from "./UserSearch";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 function App() {
-  const [repos, setRepos] = useState([]);
-  const [defaultRepos, setDefaultRepos] = useState([]);
-  const [fetchStatus, setFetchStatus] = useState("RESOLVED");
-  const [fetchError, setFetchError] = useState("");
+  const [state, dispatch] = useReducer(appReducer, {
+    repos: [],
+    defaultRepos: [],
+    fetchStatus: "RESOLVED",
+    fetchError: ""
+  });
   const fetchRepos = async username => {
     try {
-      setFetchStatus("PENDING");
+      dispatch({ type: "FETCH_STATUS", status: "PENDING" });
       const resp = await fetch(
         `https://api.github.com/users/${username}/repos`
       ).then(r => {
@@ -21,11 +23,11 @@ function App() {
           return r.json();
         }
       });
-      setRepos(resp);
-      setFetchStatus("RESOLVED");
+      dispatch({ type: "SET_REPOS", repos: resp });
+      dispatch({ type: "FETCH_STATUS", status: "RESOLVED" });
     } catch (error) {
-      setFetchStatus("ERROR");
-      setFetchError(error.message);
+      dispatch({ type: "FETCH_STATUS", status: "ERROR" });
+      dispatch({ type: "FETCH_ERROR", error: error.message });
     }
   };
   useEffect(() => {
@@ -33,16 +35,17 @@ function App() {
       const resp = await fetch("https://api.github.com/repositories").then(
         res => res.json()
       );
-      setRepos(resp);
-      setDefaultRepos(resp);
+      dispatch({ type: "SET_REPOS", repos: resp });
+      dispatch({ type: "SET_DEFAULT_REPOS", repos: resp });
     };
     fetchDefaults();
   }, []);
   const reset = () => {
-    setRepos(defaultRepos);
-    setFetchStatus("RESOLVED");
-    setFetchError("");
+    dispatch({ type: "SET_REPOS", repos: state.defaultRepos });
+    dispatch({ type: "FETCH_STATUS", status: "RESOLVED" });
+    dispatch({ type: "FETCH_ERROR", error: "" });
   };
+  const { fetchError, repos, fetchStatus } = state;
   return (
     <Container style={{ paddingTop: "3rem" }}>
       <Row style={{ marginBottom: "2rem" }}>
@@ -55,4 +58,18 @@ function App() {
   );
 }
 
+const appReducer = (state, action) => {
+  switch (action.type) {
+    case "SET_REPOS":
+      return { ...state, repos: action.repos };
+    case "SET_DEFAULT_REPOS":
+      return { ...state, defaultRepos: action.repos };
+    case "FETCH_STATUS":
+      return { ...state, fetchStatus: action.status };
+    case "FETCH_ERROR":
+      return { ...state, fetchError: action.error };
+    default:
+      return state;
+  }
+};
 export default App;
